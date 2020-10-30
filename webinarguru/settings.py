cd51eb13dfdf5de12194213601a508706e42074e
tomfrankkirk <tomfrankkirk@gmail.com>
2020-10-30 20:21:29 +0000

    rm ref to background_task

--- webinarguru/settings.py
+++ webinarguru/settings.py
@@ -43,7 +43,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'event_finder.apps.event_finderConfig',
-    'background_task',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
