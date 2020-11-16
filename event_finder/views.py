2e3b7a4ed4893becd64bb8e510d343ac8f6e4865
tomfrankkirk <tomfrankkirk@gmail.com>
2020-11-16 19:33:35 +0000

    BF ICS android formatting

--- event_finder/views.py
+++ event_finder/views.py
@@ -1,11 +1,8 @@
-from datetime import date
-from itertools import repeat
-from django.forms.formsets import all_valid
 from django.shortcuts import render
 from django.http import HttpResponse
-import json 
 import ics
 import datetime
+import codecs
 
 from .models import Event
 from .db_updates import time_now
@@ -29,15 +26,19 @@ def index(request):
 
 def detail(request, id):
     event = Event.objects.get(tweet_id=id)
-    c = ics.Calendar()
-    e = ics.Event()
-    e.name = event.title
-    e.begin = event.datetime.strftime("%Y-%m-%d %H:%M")
+    dt_format = "%Y-%m-%d %H:%M"
     end = event.datetime + datetime.timedelta(hours=1)
-    e.end = end.strftime("%Y-%m-%d %H:%M")
-    e.url = event.link 
+    c = ics.Calendar()
+    e = ics.Event(
+        name=event.title, 
+        begin=event.datetime.strftime(dt_format), 
+        end=end.strftime(dt_format),
+        created=datetime.datetime.now().strftime(dt_format),
+        url=event.link 
+    )
     c.events.add(e)
 
-    response = HttpResponse(str(c), content_type="application/ics")
+    cal_str = codecs.encode(str(c), encoding='utf-8')
+    response = HttpResponse(cal_str, content_type="application/ics")
     response['Content-Disposition'] = 'inline; filename=%s.ics' % e.name
     return response
